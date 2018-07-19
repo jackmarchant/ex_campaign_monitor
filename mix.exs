22d65d5627ee6b7198829de63fc238f573b32957
Jack Marchant <jack@expert360.com>
2018-07-19 23:29:38 +1000

    update version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExCampaignMonitor.MixProject do
   def project do
     [
       app: :ex_campaign_monitor,
-      version: "0.1.3",
+      version: "0.1.4",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
