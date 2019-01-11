95a8fa0f70005ae9ca0b5a33914e1239ebb5ccfd
Jack Marchant <jack@vamp.me>
2019-01-11 17:44:05 +1100

    add support for sending smart emails

---
+++ lib/ex_campaign_monitor/transactional.ex
@@ -0,0 +1,19 @@
+defmodule ExCampaignMonitor.Transactional do
+  @moduledoc """
+  Manage Transactional Emails
+  Reference: https://www.campaignmonitor.com/api/transactional
+  """
+
+  alias ExCampaignMonitor.Transport
+  alias ExCampaignMonitor.Transactional.SmartEmail
+
+  @doc "Send a new Smart Email"
+  def send_smart_email(smart_email_id, data) do
+    "/transactional/smartEmail/#{smart_email_id}/send"
+    |> Transport.request(:post, SmartEmail.to_cm(data))
+    |> case do
+      {:ok, response} -> {:ok, SmartEmail.from_cm(response)}
+      {:error, _} = error -> error
+    end
+  end
+end
