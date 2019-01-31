c9e9eabad633b0e1efa116bcb62c778afd8f8660
Jack Marchant <jack@vamp.me>
2019-01-31 11:28:03 +1100

    publish v0.9.4

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExCampaignMonitor.MixProject do
   def project do
     [
       app: :ex_campaign_monitor,
-      version: "0.9.3",
+      version: "0.9.4",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
